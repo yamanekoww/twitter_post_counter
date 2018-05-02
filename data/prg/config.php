754245e3861e092c9598a18aa2becaecde89c5c1
ME_Testa <yamanekoww@gmail.com>
2018-05-02 09:27:57 +0900

    Update config.php

--- data/prg/config.php
+++ data/prg/config.php
@@ -1,6 +1,6 @@
 <?php
 
 // ツイッターにアクセスするためのパラメータをセット
-define('CONSUMER_KEY', 'yc0cefEvAIP1U5h4vpkd4DzQb');
-define('CONSUMER_SECRET', '0Ix0iWDcHyRiudzBByU3z927eKcUGMCJKgx2gfnJd3eLggF8n8');
-define( 'OAUTH_CALLBACK', 'http://demo.skword.com/takasu_files/twitter_post_counter/callback.php' );
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define( 'OAUTH_CALLBACK', '' );
